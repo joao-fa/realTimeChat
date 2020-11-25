fdf6cfdc713ecc67e7a7a32213a110f4cc383336
João Araujo <joaovicttor_a@usp.br>
2020-11-25 20:01:32 -0300

    gitignore, procfile and chat.js changes

--- webapp/src/components/Chat/Chat.js
+++ webapp/src/components/Chat/Chat.js
@@ -13,7 +13,7 @@ const Chat = ({location}) => {
     const [room, setRoom] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://real-time-react-chat-appl.herokuapp.com/';
 
     useEffect(() => {
         const {name, room} = queryString.parse(location.search);
